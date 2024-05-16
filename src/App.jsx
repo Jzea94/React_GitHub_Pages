e6a6d8ef2e2dec30e058464f6c610d240fa073e9
Jzea94 <jjzea.94@gmail.com>
2024-05-16 00:02:16 -0500

    corriendo ruta en vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import TicTacToe from './components/Game/Game.jsx'
+// import TicTacToe from './components/TicTacToe/TicTacToe.jsx'
 // import PointerFollow from './components/PointerMove/Pointer.jsx'
 // import FollowButton from './components/Button/Button'
-// import TicTacToe from './components/Game/Game.jsx'
 // import ButtonComponent from './components/ButtonTest/ButtonTest.jsx'
-import TicTacToe from './components/TicTacToe/TicTacToe.jsx'
 import './App.css'
 
 function App() {
