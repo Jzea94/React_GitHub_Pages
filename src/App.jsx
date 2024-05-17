57dac8b9781959737b32484a5b52c6e50e71d4cd
Jzea94 <jjzea.94@gmail.com>
2024-05-17 04:37:06 -0500

    se implemento el seguimiento del puntero y la posición random del punto cada cierto tiempo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // import TicTacToe from './components/Game/Game.jsx'
 // import TicTacToe from './components/TicTacToe/TicTacToe.jsx'
-import EnabledPointerFollow from './components/PointerMove/Pointer.jsx'
 // import FollowButton from './components/Button/Button'
 // import ButtonComponent from './components/ButtonTest/ButtonTest.jsx'
 import './App.css'
+import FollowMouse from './components/PointerMove/Pointer.jsx'
 
 function App() {
   return (
     <>
       {/* <TicTacToe/> */}
-      <EnabledPointerFollow/>
+      <FollowMouse/>
     </>
   )
 }
--- src/components/PointerMove/Pointer.jsx
+++ src/components/PointerMove/Pointer.jsx
@@ -4,6 +4,21 @@ import './Pointer.css'
 const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [pointPosition, setPointPosition] = useState({ x: 0, y: 0 })
+
+  // random point
+  useEffect(() => {
+    let timer
+    if (enabled) {
+      timer = setTimeout(() => {
+        const positionX = Math.floor(Math.random() * 889)
+        const positionY = Math.floor(Math.random() * 589)
+        setPointPosition({ x: positionX, y: positionY})
+      }, 2000 )
+    }
+
+    return () => clearTimeout(timer)
+  }, [pointPosition, enabled])
 
   // pointer move
   useEffect(() => {
@@ -40,77 +55,46 @@ const FollowMouse = () => {
     }
   }, [enabled])
 
-  return (
-    <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
-      />
-      <button id='pointer-follow' onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
-      </button>
-    </>
-  )
-}
-
-
-// Esta logica se hizo con el fin de agregar un button para desmontar el componente
-function EnabledPointerFollow () {
-  const [montarComponente, setMontarComponente] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0})
-  // const [score, setScore] = useState(0)
-
-  // const pointerPosition = (event) => {
-  //   const {positionX, positionY} = event
-  // }
-  // const countScore = () => {
-  //   window.addEventListener('pointermove', pointerPosition)
-  //   position == 5 && setScore(score + 1)
-  // }
-
-
-  setTimeout(() => {
-    const positionX = Math.floor(Math.random() * 889) 
-    const positionY = Math.floor(Math.random() * 589)
-    setPosition({ x: positionX, y: positionY})
-  }, 3000)
+  // TODO: no permitir que el circulo salga del div
 
   return (
     <main>
       <section id='game-board'>
-        <div
-          id='point'
-          style={{
+      <div style={{
           border: '1px solid #fff',
           borderRadius: '50%',
           position: 'relative',
-          top: position.y,
-          left: position.x,
-          width:10,
+          top: pointPosition.y,
+          left: pointPosition.x,
+          width: 10,          
           height: 10,
-          backgroundColor: '#fff',
-        }} >
-        </div>
+          backgroundColor: '#fff'
+        }}/>
+
+        <div style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}/>
+
       </section>
 
-      <div id='score'>Score: {`${0}`}</div>
 
-      { montarComponente && <FollowMouse /> }
-      <button id='montar-componente' onClick={() => setMontarComponente(!montarComponente)}>
-        { montarComponente ? 'Desmontar' : 'Montar' } componente
-      </button>      
+      <button id='pointer-follow' onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+
+      <span id='score'>Score: {`${0}`}</span>
     </main>
   )
 }
 
-export default EnabledPointerFollow
+export default FollowMouse
