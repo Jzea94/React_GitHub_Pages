f2d4b7d17a44c33b6001f714b96df8b1f5d6e823
Jzea94 <jjzea.94@gmail.com>
2024-05-18 04:38:10 -0500

    se implemento sistema de puntos y se mejoraron los estilos, ademas se restringio el movimiento del pointer fuera del game-board

--- src/components/PointerMove/Pointer.jsx
+++ src/components/PointerMove/Pointer.jsx
@@ -1,31 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './Pointer.css'
 
 const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
-  const [pointPosition, setPointPosition] = useState({ x: 0, y: 0 })
-
-  // random point
+  const [pointPosition, setPointPosition] = useState({ x: 150, y: 150 })
+  const [score, setScore] = useState(0)
+  const gameBoardRef = useRef(null); // Reference to the game-board
+  
+  // Logica para generar la posición random del punto
   useEffect(() => {
     let timer
     if (enabled) {
       timer = setTimeout(() => {
-        const positionX = Math.floor(Math.random() * 889)
-        const positionY = Math.floor(Math.random() * 589)
+        const positionX = Math.floor(Math.random() * 869)
+        const positionY = Math.floor(Math.random() * 569)
         setPointPosition({ x: positionX, y: positionY})
-      }, 2000 )
+      }, 1000 )
     }
 
     return () => clearTimeout(timer)
   }, [pointPosition, enabled])
 
-  // pointer move
+  // Logica para capturar la posición del cursor en la pantalla
   useEffect(() => {
-
     const handleMove = (event) => {
       const { clientX, clientY } = event
-      setPosition({ x: clientX, y: clientY })
+      const gameBoard = gameBoardRef.current.getBoundingClientRect();
+      const x = Math.max(27, Math.min(clientX - gameBoard.left, gameBoard.width - 27));
+      const y = Math.max(27, Math.min(clientY - gameBoard.top, gameBoard.height - 27));
+      setPosition({ x, y });
     }
 
     if (enabled) {
@@ -48,51 +52,63 @@ const FollowMouse = () => {
 
   // change body className
   useEffect(() => {
-    document.body.classList.toggle('no-cursor', enabled)
+    const cursorElement = document.getElementById('cursor')
+    cursorElement.classList.toggle('no-cursor', !enabled)
 
     return () => {
-      document.body.classList.remove('no-cursor')
+      cursorElement.classList.remove('no-cursor')
     }
   }, [enabled])
 
-  // TODO: no permitir que el circulo salga del div
+  // Logica para validar que el punto este dentro del cursor
+  function count () {
+    const newScore = score
+    setScore(() => newScore + 1)
+  }
+
+  // Reset the game
+  const resetGame = () => {
+    setScore(0);
+    setPointPosition({ x: 150, y: 150 });
+    setPosition({ x: 0, y: 0 });
+    setEnabled(false);
+  };
+  
+  // TODO: no permitir que el div id=cursor salga del game board
 
   return (
     <main>
-      <section id='game-board'>
-      <div style={{
-          border: '1px solid #fff',
-          borderRadius: '50%',
-          position: 'relative',
+      <section id='game-board' ref={gameBoardRef}>
+      <div
+          className='randomPoint'
+          onClick={count}
+          style={{
           top: pointPosition.y,
-          left: pointPosition.x,
-          width: 10,          
-          height: 10,
-          backgroundColor: '#fff'
+          left: pointPosition.x
         }}/>
 
-        <div style={{
-          position: 'absolute',
+        <div id='cursor'
+          style={{
+          position: 'relative',
           backgroundColor: 'rgba(0, 0, 0, 0.5)',
           border: '1px solid #fff',
           borderRadius: '50%',
           opacity: 0.8,
           pointerEvents: 'none',
-          left: -25,
-          top: -25,
+          left: -27,
+          top: -59,
           width: 50,
           height: 50,
           transform: `translate(${position.x}px, ${position.y}px)`
         }}/>
 
       </section>
-
-
-      <button id='pointer-follow' onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      
+      <button id='pointer-follow' onClick={() => enabled ? resetGame() : setEnabled(true)}>
+        {enabled ? 'End' : 'Start'} Game
       </button>
 
-      <span id='score'>Score: {`${0}`}</span>
+      <span id='score'>Score: {`${score}`}</span>
     </main>
   )
 }
